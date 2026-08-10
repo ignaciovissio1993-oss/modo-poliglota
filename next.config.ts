e15dec4cfb7db713ef2563a96cc8042f984dfbc7
Ignacio <ignaciovissio1993@gmail.com>
2026-08-10 15:34:30 -0300

    Fix custom domain deployment

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/modo-poliglota",
   images: {
     unoptimized: true,
     remotePatterns: [
